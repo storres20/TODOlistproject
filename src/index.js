994fec09af6865c2f94b5007f32df61da5799588
storres20 <sanchotorres777@gmail.com>
2022-11-03 16:09:55 -0500

    Add javascript file for task

--- src/index.js
+++ src/index.js
@@ -1,36 +1,69 @@
 import './style.css';
 import './imgs/bg1.png';
-
-const arrTodo = [
-  {
-    description: 'do homework',
-    completed: true,
-    index: 3,
-  },
-  {
-    description: 'clean house',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'play soccer',
-    completed: false,
-    index: 2,
-  },
-];
+import Task from '../modules/ntask.js';
+import addtask from '../modules/addtask.js';
+import removetask from '../modules/removetask.js';
+import edittask from '../modules/edittask.js';
 
 const todoList = document.getElementById('todoList');
 
-arrTodo.sort((a, b) => a.index - b.index);
+// displaying tasks on window loading
+window.addEventListener('DOMContentLoaded', () => {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
 
-arrTodo.forEach((item) => {
-  todoList.innerHTML += `
-    <div class="todoFlex">
-      <div class="todoDiv">
-        <input type="checkbox" name="check" id="" ${item.completed ? 'checked' : ''} >
-        <p>${item.description}</p>
-      </div>
-      <i class="bi bi-three-dots-vertical"></i>
-    </div>
-  `;
+  if (tasks !== null) {
+    tasks.sort((a, b) => a.index - b.index);
+
+    tasks.forEach((item) => {
+      todoList.innerHTML += `
+        <div class="todoFlex">
+          <div class="todoDiv">
+            <input type="checkbox" name="${item.id}" class="checkbox" ${item.completed ? 'checked' : ''} >
+            <input class="todoP" name="${item.id}" value="${item.description}" >
+          </div>
+          <i class="bi bi-three-dots-vertical dots"></i>
+        </div>
+      `;
+    });
+  }
 });
+
+// formInput + enter
+const formInput = document.querySelector('.formInput');
+const enter = document.getElementById('enter');
+
+formInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+  }
+});
+
+let ind;
+enter.addEventListener('click', () => {
+  if (localStorage.getItem('tasks') === null) {
+    ind = 1;
+  } else {
+    ind = JSON.parse(localStorage.getItem('tasks')).length + 1;
+  }
+
+  const todo1 = new Task(formInput.value, false, ind, Date.now().toString());
+  addtask(todo1); // add new task
+  formInput.value = '';
+});
+
+// clear all completed button
+const clearAll = document.querySelector('.clearAll');
+
+clearAll.addEventListener('click', () => {
+  const checkbox = document.querySelectorAll('.checkbox:nth-child(1)');
+
+  checkbox.forEach((item) => {
+    if (item.checked === true) {
+      removetask(item.name); // remove task
+      item.parentElement.parentElement.remove();
+    }
+  });
+});
+
+// edit task
+edittask();
